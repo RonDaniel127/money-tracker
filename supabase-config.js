25fa03ac9c6f608ea925147b905d4babfd856330
Ron Daniel Ligdao <rondanielligdao@gmail.com>
2026-09-12 19:36:47 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://lnnaepqzmzphoagkujvd.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_pUYhhgc73lAbLEa3ncYSKw_ZNDbZtja';
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
